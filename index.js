3844873f40ac2d76556b1618d8c4bc36fe2d1818
marcelo.cabral <mneresc@gmail.com>
2018-11-19 22:38:29 -0200

    [master][upd] Add err debug

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ const tr = require('tor-request');
 tr.request('https://pt.wikipedia.org/wiki/Lista_de_epis%C3%B3dios_de_Naruto_Shippuden', function (err, res, body) {
   if (!err && res.statusCode == 200) {
     console.log(body);
+  }else{
+    console.log(err);
   }
 });
